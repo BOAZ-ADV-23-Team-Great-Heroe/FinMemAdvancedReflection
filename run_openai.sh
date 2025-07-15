0c5b3ec7a9e4200585184ea0b6e3b26b7527ffc6
withmochaa <112954275+withmochaa@users.noreply.github.com>
2025-07-16 00:56:33 +0900

    Overwrite with local state

--- run_openai.sh
+++ run_openai.sh
@@ -5,12 +5,13 @@ set -e
 # --- 파라미터 설정 ---
 START_DATE=${1:-"2021-11-16"}
 END_DATE=${2:-"2021-11-17"}
-OUTPUT_PATH=${3:-"data/05_model_output/nvda_4o_mini_run"}
+OUTPUT_PATH=${3:-"data/05_model_output/tsla_4o_mini_run"}
 
 
 # 사용할 설정 파일과 데이터 파일 경로
-CONFIG_FILE="config/nvda_gpt_config.toml"
-MARKET_DATA_FILE="data/03_model_input/add_filing_nvda.pkl"
+CONFIG_FILE="config/tsla_gpt_config.toml"
+MARKET_DATA_FILE="data/03_model_input/add_filing_tsla.pkl"
+
 
 
 echo "=================================================="
@@ -38,8 +39,11 @@ echo "Final results are saved in: $OUTPUT_PATH"
 echo "=================================================="
 
 
- python save_file.py \
-    --output-path data/05_model_output/nvda_4o_mini_run \
-    --portfolio-out nvda_portfolio_4o_mini.csv \
-    --analysis-out nvda_full_report__4o_mini.json \
-    --performance-out nvda_performance__4o_mini.json
+TRADING_SYMBOL=$(basename "$CONFIG_FILE" | cut -d'_' -f1)
+
+# 추출된 심볼과 OUTPUT_PATH 변수를 사용하여 동적으로 파일 이름을 생성합니다.
+python save_file.py \
+    --output-path "$OUTPUT_PATH" \
+    --portfolio-out "${TRADING_SYMBOL}_portfolio_4o_mini.csv" \
+    --analysis-out "${TRADING_SYMBOL}_full_report_4o_mini.json" \
+    --performance-out "${TRADING_SYMBOL}_performance_4o_mini.json"
